09ff6fcb7dd327690fb169f023810271ec037954
MandasK <king.amanda.r@gmail.com>
2020-08-21 12:02:43 -0500

    placeNames js added

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,17 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+  console.log("All Place Names")
+  for (let name of names) {
+    console.log(`New place, ${name}`)
+  }
+  const theNames = names.filter(n => n.startsWith("The"))
+  console.log(theNames)
 
+  for (let name of theNames) {
+    console.log(`the places, ${name}`)
+  }
 }
 
 main();
